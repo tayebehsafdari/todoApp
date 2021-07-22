cbeeb27eb6d692eb378aafaff61f8deb8719de5a
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-07-22 14:54:40 +0430

    modify Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,24 +7,12 @@ function Counter({initialCount}) {
         setCount(event.target.value);
     }
 
-    function handleReset() {
-        setCount(initialCount);
-    }
-
-    function handleDecrement() {
-        setCount(prevCount => prevCount - 1);
-    }
-
-    function handleIncrement() {
-        setCount(prevCount => prevCount + 1);
-    }
-
     return (
         <div className="input-group my-5">
-            <span className="input-group-text" onClick={handleReset}>Reset</span>
-            <span className="input-group-text" onClick={handleDecrement}>-</span>
+            <span className="input-group-text" onClick={() => setCount(initialCount)}>Reset</span>
+            <span className="input-group-text" onClick={() => setCount(count - 1)}>-</span>
             <input type="text" className="form-control" value={count} onChange={handleChange}/>
-            <span className="input-group-text" onClick={handleIncrement}>+</span>
+            <span className="input-group-text" onClick={() => setCount(count + 1)}>+</span>
         </div>
     );
 }
