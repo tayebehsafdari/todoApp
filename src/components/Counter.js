9f4176dd36801bed55761f0684c81443732d2f29
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-07-24 15:01:05 +0430

    modify files

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,15 +3,12 @@ import React, {useState} from "react";
 function Counter({initialCount}) {
     const [count, setCount] = useState(initialCount);
 
-    function handleChange(event) {
-        setCount(event.target.value);
-    }
-
     return (
         <div className="input-group my-5">
             <span className="input-group-text" onClick={() => setCount(initialCount)}>Reset</span>
             <span className="input-group-text" onClick={() => setCount(count - 1)}>-</span>
-            <input type="text" className="form-control" value={count} onChange={handleChange}/>
+            <input type="text" className="form-control" value={count}
+                   onChange={(e) => setCount(e.target.value)}/>
             <span className="input-group-text" onClick={() => setCount(count + 1)}>+</span>
         </div>
     );
