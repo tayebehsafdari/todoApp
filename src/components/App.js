5c9c5d0b578699d095fa2b8c806e228804f3da58
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-07-25 15:26:33 +0430

    modify files

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import './App.scss';
 
-import {Container, Form} from 'react-bootstrap';
-import Counter from "./Counter";
+import {Container} from 'react-bootstrap';
+import Timer from "./Timer";
+
 
 function App() {
     return (
         <div className="App">
             <Container>
-                <Form>
-                    <Counter initialCount={0}/>
-                </Form>
+                <Timer/>
             </Container>
         </div>
     );
